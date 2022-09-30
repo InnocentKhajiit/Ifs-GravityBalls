ae1e2da756a689d1e260c7dffee5ef03a70b0246
InnocentKhajiit <alekses2001@gmail.com>
2022-09-30 16:48:00 +0500

    Добавьте файлы проекта.

---
+++ WorldModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GravityBalls
+{
+	public class WorldModel
+	{
+		public double BallX;
+		public double BallY;
+		public double BallRadius;
+		public double WorldWidth;
+		public double WorldHeight;
+
+        public const double HorizontalSpeed = 100;
+        public const double VerticalSpeed = 200;
+		public void SimulateTimeframe(double dt)
+        {
+            var dx = HorizontalSpeed * dt;
+			var dy = VerticalSpeed * dt;
+            var newX = dx < 0 ? Math.Max(BallRadius, BallX + dx) : Math.Min(WorldWidth - BallRadius, BallX + dx);
+			var newY = dy < 0 ? Math.Max(BallRadius, BallY + dy) : Math.Min(WorldHeight - BallRadius, BallY + dy);
+            if (newX == BallX || newY == BallY) 
+                return;
+            BallX = newX;
+            BallY = newY;
+        }
+	}
+}
